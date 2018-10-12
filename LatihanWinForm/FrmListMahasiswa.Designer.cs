f1a5baefae742c150700770a7db7c427ef8308f6
Nurul Afini <43399744+nurulafini@users.noreply.github.com>
2018-10-12 20:24:59 +0700

    Add files via upload

---
+++ LatihanWinForm/FrmListMahasiswa.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace LatihanWinForm
+{
+    partial class FrmListMahasiswa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dtpTanggalLahir = new System.Windows.Forms.DateTimePicker();
+            this.txtTempatLahir = new System.Windows.Forms.TextBox();
+            this.rdoPerempuan = new System.Windows.Forms.RadioButton();
+            this.rdoLakilaki = new System.Windows.Forms.RadioButton();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.mskNpm = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnTutup = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lvwMahasiswa = new System.Windows.Forms.ListView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dtpTanggalLahir);
+            this.groupBox1.Controls.Add(this.txtTempatLahir);
+            this.groupBox1.Controls.Add(this.rdoPerempuan);
+            this.groupBox1.Controls.Add(this.rdoLakilaki);
+            this.groupBox1.Controls.Add(this.txtNama);
+            this.groupBox1.Controls.Add(this.mskNpm);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(289, 207);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "[ Data Mahasiswa ]";
+            // 
+            // dtpTanggalLahir
+            // 
+            this.dtpTanggalLahir.CustomFormat = "dd/MM/yyyy";
+            this.dtpTanggalLahir.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpTanggalLahir.Location = new System.Drawing.Point(86, 140);
+            this.dtpTanggalLahir.Name = "dtpTanggalLahir";
+            this.dtpTanggalLahir.Size = new System.Drawing.Size(100, 20);
+            this.dtpTanggalLahir.TabIndex = 10;
+            // 
+            // txtTempatLahir
+            // 
+            this.txtTempatLahir.Location = new System.Drawing.Point(86, 114);
+            this.txtTempatLahir.Name = "txtTempatLahir";
+            this.txtTempatLahir.Size = new System.Drawing.Size(100, 20);
+            this.txtTempatLahir.TabIndex = 9;
+            // 
+            // rdoPerempuan
+            // 
+            this.rdoPerempuan.AutoSize = true;
+            this.rdoPerempuan.Location = new System.Drawing.Point(186, 87);
+            this.rdoPerempuan.Name = "rdoPerempuan";
+            this.rdoPerempuan.Size = new System.Drawing.Size(79, 17);
+            this.rdoPerempuan.TabIndex = 8;
+            this.rdoPerempuan.TabStop = true;
+            this.rdoPerempuan.Text = "Perempuan";
+            this.rdoPerempuan.UseVisualStyleBackColor = true;
+            // 
+            // rdoLakilaki
+            // 
+            this.rdoLakilaki.AutoSize = true;
+            this.rdoLakilaki.Location = new System.Drawing.Point(86, 87);
+            this.rdoLakilaki.Name = "rdoLakilaki";
+            this.rdoLakilaki.Size = new System.Drawing.Size(64, 17);
+            this.rdoLakilaki.TabIndex = 7;
+            this.rdoLakilaki.TabStop = true;
+            this.rdoLakilaki.Text = "Laki-laki";
+            this.rdoLakilaki.UseVisualStyleBackColor = true;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(86, 59);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(185, 20);
+            this.txtNama.TabIndex = 6;
+            // 
+            // mskNpm
+            // 
+            this.mskNpm.Location = new System.Drawing.Point(86, 32);
+            this.mskNpm.Mask = "##,##,####";
+            this.mskNpm.Name = "mskNpm";
+            this.mskNpm.Size = new System.Drawing.Size(100, 20);
+            this.mskNpm.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tanggal Lahir";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tempat Lahir";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Jenis Kelamin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NPM";
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(21, 241);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(75, 23);
+            this.btnSimpan.TabIndex = 1;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(123, 241);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(75, 23);
+            this.btnHapus.TabIndex = 11;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // btnTutup
+            // 
+            this.btnTutup.Location = new System.Drawing.Point(598, 241);
+            this.btnTutup.Name = "btnTutup";
+            this.btnTutup.Size = new System.Drawing.Size(75, 23);
+            this.btnTutup.TabIndex = 12;
+            this.btnTutup.Text = "Tutup";
+            this.btnTutup.UseVisualStyleBackColor = true;
+            this.btnTutup.Click += new System.EventHandler(this.btnTutup_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lvwMahasiswa);
+            this.groupBox2.Location = new System.Drawing.Point(307, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(372, 207);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "[ Daftar Mahasiswa ]";
+            // 
+            // lvwMahasiswa
+            // 
+            this.lvwMahasiswa.Location = new System.Drawing.Point(6, 19);
+            this.lvwMahasiswa.Name = "lvwMahasiswa";
+            this.lvwMahasiswa.Size = new System.Drawing.Size(360, 182);
+            this.lvwMahasiswa.TabIndex = 0;
+            this.lvwMahasiswa.UseCompatibleStateImageBehavior = false;
+            this.lvwMahasiswa.SelectedIndexChanged += new System.EventHandler(this.lvwMahasiswa_SelectedIndexChanged);
+            // 
+            // FrmListMahasiswa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 331);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnTutup);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "FrmListMahasiswa";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data Mahasiswa";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dtpTanggalLahir;
+        private System.Windows.Forms.TextBox txtTempatLahir;
+        private System.Windows.Forms.RadioButton rdoPerempuan;
+        private System.Windows.Forms.RadioButton rdoLakilaki;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.MaskedTextBox mskNpm;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnTutup;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView lvwMahasiswa;
+    }
+}
